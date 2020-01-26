63483ea3b4e7dcebaf36c14236c1358c9a6d39d5
juliaschmitt <DiamondsJules72464>
2020-01-25 19:58:12 -0600

    Adding preliminary page interactivity.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
-document.querySelector('Login').onclick = function() {
-    alert('Ouch! Stop poking me!');
+// Sign Up Button Click Event
+document.querySelector('.signupbtn').onclick = function() {
+    window.location.replace('index.html'); // needs to be replaced with sign up page
+}
+
+// Sign In Button Click Event
+document.querySelector('.signinbtn').onclick = function() {
+    window.location.replace('index.html'); // needs to be replaced with sign in page
 }
