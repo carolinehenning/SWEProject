e92d8012c745a0fdff9d5a71f55717e3b43a7f2f
juliaschmitt <DiamondsJules72464>
2020-02-24 11:05:22 -0600

    Homepage redirect to sign in and sign up pages.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+document.querySelector('.signinbtn').onclick = function() {
+    window.location.replace('views/signin.html');
+}
+
+document.querySelector('.signupbtn').onclick = function() {
+    window.location.replace('views/signup.html');
+}
